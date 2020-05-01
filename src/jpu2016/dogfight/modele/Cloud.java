897442939628c481cfcc0ed508e2acd919f765f9
Queenbee-ui <58205376+Queenbee-ui@users.noreply.github.com>
2020-05-01 15:19:54 +0100

    Phase 3

--- src/jpu2016/dogfight/modele/Cloud.java
+++ src/jpu2016/dogfight/modele/Cloud.java
@@ -3,12 +3,12 @@ package jpu2016.dogfight.modele;
 public class Cloud extends Mobile{
 	
 	public Cloud(Direction direction, Dimension dimension) {
-		super(direction, null, dimension, HEIGHT, IMAGE1);
+		super(direction, null, dimension, HEIGHT, IMAGE);
 		// TODO Auto-generated constructor stub
 	}
 	private static int SPEED = 1;
 	private static int WIDTH = 300;
 	private static int HEIGHT = 150;
-	private static String IMAGE1 = "cloud";
+	private static String IMAGE = "cloud";
 	
 }
